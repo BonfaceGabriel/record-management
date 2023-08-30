4b8fa15ae9c585d1e51b3ca8a55e58e317512a42
BonfaceGabriel <bonfacegabriel44@gmail.com>
2023-08-30 12:09:22 +0300

    decouple-removed

--- sql/settings.py
+++ sql/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("APP_SECRET_KEY")
+SECRET_KEY = 'django-insecure-bgrjw9!91%$&)dm=ilerh@v5kk^)%z53=&=q&0$616p$q(6qo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("APP_DEBUG_STATUS", cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
