7ec735eeb701f95cc488439ce80b4f54fdd844b3
BonfaceGabriel <bonfacegabriel44@gmail.com>
2023-08-30 10:32:47 +0300

    fix python-decouple

--- sql/settings.py
+++ sql/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("APP_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("APP_DEBUG_STATUS", cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
