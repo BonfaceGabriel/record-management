8a0d22d2fab307602d6d9b1b706d07544a356e34
BonfaceGabriel <bonfacegabriel44@gmail.com>
2023-12-03 22:19:22 +0300

    sql

--- sql/settings.py
+++ sql/settings.py
@@ -82,7 +82,7 @@ CORS_ALLOW_HEADERS = "*"
 # CORS_ALLOW_CREDENTIALS: True
 
 CORS_ALLOWED_ORIGINS = [
-  "http://127.0.0.1:8000",
+#   "http://127.0.0.1:8000",
   "http://localhost:8080",
   "http://localhost:3000",
   "http://localhost:8081",
