27eb8ad7690701fafc488b8f1f475f9b50a55d85
BonfaceGabriel <bonfacegabriel44@gmail.com>
2024-01-08 21:24:35 +0300

    Updated Cors Policy

--- sql/settings.py
+++ sql/settings.py
@@ -60,16 +60,16 @@ CORS_ALLOW_HEADERS = "*"
 
 # CORS_ALLOW_CREDENTIALS: True
 
-# CORS_ALLOWED_ORIGINS = [
-# #   "http://127.0.0.1:8000",
-#   "http://localhost:8080",
-#   "http://localhost:3000",
-#   "http://localhost:8081",
-#   "https://record-managent.onrender.com",
-#   "https://record-app-management.onrender.com",
-#   "https://ict-authority.onrender.com"
+CORS_ALLOWED_ORIGINS = [
+
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://localhost:8081",
+  "https://record-managent.onrender.com",
+  "https://record-app-management.onrender.com",
+  "https://ict-authority.onrender.com"
   
-# ]
+]
 
 ROOT_URLCONF = 'sql.urls'
 
